bfcf554f89c526d0a47abf49a9802fcfb39d87a9
shruti-mathews <shrutisusan@tamu.edu>
2024-06-13 12:51:30 +0400

    Formed APIs for USER model CRUD

--- backend-api/controllers/user.controller.js
+++ backend-api/controllers/user.controller.js
@@ -1,30 +1,62 @@
+import prisma from "../lib/prisma.js";
+import bcrypt from "bcrypt";
+
 export const getUsers = async (req, res) => {
   try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
   } catch (e) {
-
-    console.log(e);
-    res.status(500).json({message: "Failed to get user"})
+    res.status(500).json({ message: "Failed to get user" });
   }
 };
 export const getUser = async (req, res) => {
-    try {
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({message: "Failed to get user"})
-    }
+  const id = req.params.id;
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id },
+    });
+    res.status(200).json(user);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Failed to get user" });
+  }
 };
 export const updateUser = async (req, res) => {
-    try {
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({message: "Failed to get user"})
-    }
+  const id = req.params.id;
+  const tokenId = req.userId;
+  if (id !== tokenId)
+    return res.status(403).json({ message: "Not Authorised" });
+  const { password, avatar, ...inputs } = req.body;
+  let encryptedPassword = null;
+  try {
+    if (password) encryptedPassword = await bcrypt.hash(password, 10);
+
+    const updatedUser = await prisma.user.update({
+      where: { id },
+      data: {
+        ...(encryptedPassword && { password: encryptedPassword }),
+        ...(avatar && { avatar }),
+        ...inputs,
+      },
+    });
+    return res.status(200).json({ message: "Updated user successfully" });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Failed to get user" });
+  }
 };
 export const deleteUser = async (req, res) => {
+  const id = req.params.id;
+  const tokenId = req.userId;
+  if (id !== tokenId)
+    return res.status(403).json({ message: "Not Authorised" });
 
-    try {
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({message: "Failed to get user"})
-    }
+  try {
+    await prisma.user.delete({where: {id}});
+    return res.status(200).json({message: "Deleted User"});
+
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Failed to get user" });
+  }
 };
--- backend-api/routes/user.routes.js
+++ backend-api/routes/user.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { getUser,getUsers,deleteUser, updateUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 const router = express.Router();
 
 router.get('/', getUsers )
-router.get('/:id',getUser)
-router.put('/:id',updateUser)
-router.delete('/:id',deleteUser)
+router.get('/:id',verifyToken,getUser)
+router.put('/:id',verifyToken,updateUser)
+router.delete('/:id',verifyToken,deleteUser)
 
 
 export default router;
